1e98165040e2b35b6e82e8ef37b75c6cdd936f2b
DevHoffman <53531808+DevHoffman@users.noreply.github.com>
2021-09-19 13:30:46 -0300

    atualiza url padrão e adiciona database.php no gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = '';
-$config['base_url'] = "http://" . $_SERVER['HTTP_HOST'] . "/";
+$config['base_url'] = "http://" . $_SERVER['HTTP_HOST'] . "/dimensiona.3.0/";
 
 /*
 |--------------------------------------------------------------------------
